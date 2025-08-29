2f42afb233b310394846ecf1b21d3a1e554269fa
Thuanny Fontoura <thuannyfmartins@gmail.com>
2025-08-29 17:44:06 -0300

    included 4 other languages

--- hello.py
+++ hello.py
@@ -1,1 +1,38 @@
-print ("Hello, World!")
+#!/usr/bin/env python3
+"""Hello World Multilanguage
+
+Depending on the configured language in the system
+the program shows the corresponding message.
+
+How to use:
+
+Have LANG configured ex:
+
+    export LANG=en_US
+
+Execution:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Thuanny Martins"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour, Monde!"
+
+print (msg)
